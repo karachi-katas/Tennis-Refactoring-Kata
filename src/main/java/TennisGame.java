76b4bb5e6021504476e0d36028e81d0af3d2e6fd
lamak <lamak.qaizar@careem.com>
2020-07-18 17:11:21 +0500

    a: extracted conditions into methods

--- src/main/java/TennisGame.java
+++ src/main/java/TennisGame.java
@@ -14,19 +14,19 @@ public class TennisGame {
 
     public String getScore() {
 
-        if (P2point > FORTY && P1point >= LOVE && (P2point - P1point) >= THIRTY) {
+        if (player2HasWon()) {
             return "Win for player2";
         }
 
-        if (P1point > FORTY && P2point >= LOVE && (P1point - P2point) >= THIRTY) {
+        if (player1HasWon()) {
             return "Win for player1";
         }
 
-        if (P2point > P1point && P1point >= FORTY) {
+        if (advantageForPlayer2()) {
             return "Advantage player2";
         }
 
-        if (P1point > P2point && P2point >= FORTY) {
+        if (advantageForPlayer1()) {
             return "Advantage player1";
         }
 
@@ -41,6 +41,22 @@ public class TennisGame {
         P1res = getStringPointValueFor(P1point);
         P2res = getStringPointValueFor(P2point);
         return P1res + "-" + P2res;
+    }
+
+    private boolean advantageForPlayer1() {
+        return P1point > P2point && P2point >= FORTY;
+    }
+
+    private boolean advantageForPlayer2() {
+        return P2point > P1point && P1point >= FORTY;
+    }
+
+    private boolean player1HasWon() {
+        return P1point > FORTY && P2point >= LOVE && (P1point - P2point) >= THIRTY;
+    }
+
+    private boolean player2HasWon() {
+        return P2point > FORTY && P1point >= LOVE && (P2point - P1point) >= THIRTY;
     }
 
     private boolean deuce() {
