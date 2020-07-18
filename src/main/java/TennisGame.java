cfce5a171413dff4dda2468d6cda56ef5d2f5d8f
Sauban bin Usman <saubanbinusman@yahoo.com>
2020-07-18 17:06:56 +0500

    Refactored, cleaned and indented code

--- src/main/java/TennisGame.java
+++ src/main/java/TennisGame.java
@@ -1,14 +1,18 @@
 public class TennisGame {
-    private static final String THIRTY = "Thirty";
-    private static final String FORTY = "Forty";
+    private static final String LOVE    = "Love";
     private static final String FIFTEEN = "Fifteen";
-    private static final String LOVE = "Love";
-    private static final String DEUCE = "Deuce";
+    private static final String THIRTY  = "Thirty";
+    private static final String FORTY   = "Forty";
+
+    private static final String DEUCE   = "Deuce";
+
+    private static final String ADVANTAGE = "Advantage %s";
+    private static final String WIN       = "Win for %s";
 
     private int P1point = 0;
     private int P2point = 0;
 
-    private String[] scoreName = new String[]{ LOVE, FIFTEEN, THIRTY, FORTY };
+    private String[] scoreName = new String[] { LOVE, FIFTEEN, THIRTY, FORTY };
 
     public String getScore() {
         int maxPoints = Math.max(P1point, P2point);
@@ -21,24 +25,30 @@ public class TennisGame {
     }
 
     private String processForEarlyGame() {
-        if (P1point == P2point) {
-            if (P1point == 3) {
-                return DEUCE;
-            }
-            return scoreName[P1point] + "-All";
+        if (P1point != P2point) {
+            return scoreName[P1point] + "-" + scoreName[P2point];
         }
 
-        return scoreName[P1point] + "-" + scoreName[P2point];
+        if (P1point < 3) return scoreName[P1point] + "-All";
+        else             return DEUCE;
     }
 
     private String processForLateGame() {
         int delta = Math.abs(P1point - P2point);
 
         switch (delta) {
-            case 0: return DEUCE;
-            case 1: return "Advantage " + getHigherPlayer();
-            default: return "Win for " + getHigherPlayer();
+            case  0: return DEUCE;
+            case  1: return getAdvantageString();
+            default: return getWinString();
         }
+    }
+
+    private String getAdvantageString() {
+        return String.format(ADVANTAGE, getHigherPlayer());
+    }
+
+    private String getWinString() {
+        return String.format(WIN, getHigherPlayer());
     }
 
     private String getHigherPlayer() {
