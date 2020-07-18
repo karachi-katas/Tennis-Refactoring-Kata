0f8dd2a14b729f87359d5e076f160e9ee63ba8aa
Amir Ali <amir.ali@careem.com>
2020-07-18 16:32:10 +0500

    Decoupled logic into 2 main parts. 1. Less than 4 and above

--- src/main/java/TennisGame.java
+++ src/main/java/TennisGame.java
@@ -7,23 +7,36 @@ public class TennisGame {
     public static final String DEUCE = "Deuce";
     public int P1point = 0;
     public int P2point = 0;
+    public String[] scoreName = new String[]{LOVE, FIFTEEN, THIRTY, FORTY};
 
     public String P1res = "";
     public String P2res = "";
 
+
     public String getScore() {
+        int maxScore = Math.max(P1point, P2point);
         String score = "";
-
-        score = equalAndBelow4(score);
-        score = deuceCondition(score);
-        score = player1InitialAdvantage(score);
-        score = player2InitialAdvantage(score);
-        score = lessThan4Counting(score);
-        score = advantageConditions(score);
-        score = winningConditions(score);
-
+//        if (maxScore < 3) {
+//            if (P1point == P2point) {
+//                return P1res + "-All";
+//            }
+//            else {
+//                return P1res + "-" + P2res;
+//            }
+//            // TODO: Simple Counting Case
+//        }
+        score = below4Counting(score);
+        score  = somethingForAbove4Counts(score);
         return score;
     }
+
+    private String somethingForAbove4Counts(String score) {
+        score = deuceCondition(score);
+        score = advantageConditions(score);
+        score = winningConditions(score);
+        return score;
+    }
+
 
     private String winningConditions(String score) {
         if (P1point >= 4 && P2point >= 0 && (P1point - P2point) >= 2) {
@@ -46,89 +59,47 @@ public class TennisGame {
         return score;
     }
 
-    private String lessThan4Counting(String score) {
-        if (P1point > P2point && P1point < 4) {
-            if (P1point == 2)
-                P1res = THIRTY;
-            if (P1point == 3)
-                P1res = FORTY;
-            if (P2point == 1)
-                P2res = FIFTEEN;
-            if (P2point == 2)
-                P2res = THIRTY;
-            score = P1res + "-" + P2res;
-        }
-
-        if (P2point > P1point && P2point < 4) {
-            if (P2point == 2)
-                P2res = THIRTY;
-            if (P2point == 3)
-                P2res = FORTY;
-            if (P1point == 1)
-                P1res = FIFTEEN;
-            if (P1point == 2)
-                P1res = THIRTY;
-            score = P1res + "-" + P2res;
-        }
-        return score;
-    }
-
-    private String player2InitialAdvantage(String score) {
-        if (P2point > 0 && P1point == 0) {
-            if (P2point == 1)
-                P2res = FIFTEEN;
-            if (P2point == 2)
-                P2res = THIRTY;
-            if (P2point == 3)
-                P2res = FORTY;
-
-            P1res = LOVE;
-            score = P1res + "-" + P2res;
-        }
-        return score;
-    }
-
-    private String player1InitialAdvantage(String score) {
-        if (P1point > 0 && P2point == 0) {
-            if (P1point == 1)
-                P1res = FIFTEEN;
-            if (P1point == 2)
-                P1res = THIRTY;
-            if (P1point == 3)
-                P1res = FORTY;
-
-            P2res = LOVE;
-            score = P1res + "-" + P2res;
-        }
-        return score;
-    }
-
     private String deuceCondition(String score) {
         if (P1point == P2point && P1point >= 3)
             score = DEUCE;
         return score;
     }
 
-    private String equalAndBelow4(String score) {
-        if (P1point == P2point && P1point < 4) {
-            if (P1point == 0)
-                score = LOVE;
-            if (P1point == 1)
-                score = FIFTEEN;
-            if (P1point == 2)
-                score = THIRTY;
-            score += "-All";
+    private String below4Counting(String score) {
+        int maxScore = Math.max(P1point, P2point);
+        if (maxScore <= 3) {
+            if (P1point == P2point) {
+                if (P1point == 3) {
+                    return DEUCE;
+                }
+                return scoreName[P1point] + "-All";
+            } else {
+
+                return scoreName[P1point] + "-" + scoreName[P2point];
+            }
         }
-
-        return score;
+        return "";
     }
 
-    void incrementPlayer1Score()
-    {
+    void incrementPlayer1Score() {
         P1point++;
+//        if (P1point < 3) {
+//            P1res = scoreName[P1point];
+//        }
+//        else {
+//            if (P1point == P2point) {
+//                P1res = DEUCE;
+//                P2res = DEUCE;
+//            } else {
+//
+//            }
+//        }
+
+
     }
-    void incrementPlayer2Score()
-    {
+
+    void incrementPlayer2Score() {
         P2point++;
+//        P2res = scoreName[P2point];
     }
 }
