dd16afd91db61711a2d65e0669c2343c8f430c76
Saad Ismail <saad.ismail@careem.com>
2020-07-18 16:55:16 +0500

    Introduce scoreString in Player and use that to generate result string

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -19,69 +19,34 @@ public class Game {
             return "Win for player2";
         }
 
-        else if (player1.hasAdvantangeOver(player2)) {
+        else if (player1.hasAdvantageOver(player2)) {
             return "Advantage player1";
         }
-        else if (player2.hasAdvantangeOver(player1)) {
+        else if (player2.hasAdvantageOver(player1)) {
             return "Advantage player2";
         }
 
         else if (player1.isLoveOver(player2)) {
-            if (player1.getPoints() == 1)
-                P1res = "Fifteen";
-            if (player1.getPoints() == 2)
-                P1res = "Thirty";
-            if (player1.getPoints() == 3)
-                P1res = "Forty";
-
-            P2res = "Love";
-            return P1res + "-" + P2res;
+            return String.format("%s-Love", player1.getPointsString());
         }
-        else if (player2.isLoveOver(player1)) {
-            if (player2.getPoints() == 1)
-                P2res = "Fifteen";
-            if (player2.getPoints() == 2)
-                P2res = "Thirty";
-            if (player2.getPoints() == 3)
-                P2res = "Forty";
 
-            P1res = "Love";
-            return P1res + "-" + P2res;
+        else if (player2.isLoveOver(player1)) {
+            return String.format("Love-%s", player2.getPointsString());
         }
 
         else if (player1.isNotDeuceAndGreaterThan(player2)) {
-            if (player1.getPoints() == 2)
-                P1res = "Thirty";
-            if (player1.getPoints() == 3)
-                P1res = "Forty";
-            if (player2.getPoints() == 1)
-                P2res = "Fifteen";
-            if (player2.getPoints() == 2)
-                P2res = "Thirty";
-            return P1res + "-" + P2res;
+            return String.format("%s-%s", player1.getPointsString(), player2.getPointsString());
         }
         else if (player2.isNotDeuceAndGreaterThan(player1)) {
-            if (player2.getPoints() == 2)
-                P2res = "Thirty";
-            if (player2.getPoints() == 3)
-                P2res = "Forty";
-            if (player1.getPoints() == 1)
-                P1res = "Fifteen";
-            if (player1.getPoints() == 2)
-                P1res = "Thirty";
-            return P1res + "-" + P2res;
+            return String.format("%s-%s", player1.getPointsString(), player2.getPointsString());
         }
 
-        else if (player1.isDeuceWith(player2))
+        else if (player1.isDeuceWith(player2)) {
             return "Deuce";
+        }
 
         else if (player1.isNotDeuceAndEqualScoreWith(player2)) {
-            if (player1.getPoints() == 0)
-                return "Love-All";
-            if (player1.getPoints() == 1)
-                return "Fifteen-All";
-            if (player1.getPoints() == 2)
-                return "Thirty-All";
+            return String.format("%s-All", player1.getPointsString());
         }
 
         return "";
--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,4 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Player {
+    private static Map<Integer, String> pointMap = new HashMap<Integer, String>() {{
+        put(0, "Love");
+        put(1, "Fifteen");
+        put(2, "Thirty");
+        put(3, "Forty");
+    }};
+
     private String name;
     private int points;
 
@@ -19,26 +29,30 @@ public class Player {
     }
 
     boolean isWinnerOver(Player other) {
-        return getPoints() >= 4 && other.getPoints() >= 0 && (getPoints() - other.getPoints()) >= 2;
+        return points >= 4 && other.points >= 0 && (points - other.points) >= 2;
     }
 
-    boolean hasAdvantangeOver(Player other) {
-        return getPoints() > other.getPoints() && other.getPoints() >= 3;
+    boolean hasAdvantageOver(Player other) {
+        return points > other.points && other.points >= 3;
     }
 
     boolean isLoveOver(Player other) {
-        return getPoints() > 0 && other.getPoints() == 0;
+        return points > 0 && other.points == 0;
     }
 
     boolean isDeuceWith(Player other) {
-        return getPoints() == other.getPoints() && getPoints() >= 3;
+        return points == other.points && points >= 3;
     }
 
     boolean isNotDeuceAndEqualScoreWith(Player other) {
-        return getPoints() == other.getPoints() && getPoints() < 4;
+        return points == other.points && points < 4;
     }
 
     boolean isNotDeuceAndGreaterThan(Player other) {
-        return getPoints() > other.getPoints() && getPoints() < 4;
+        return points > other.points && points < 4;
+    }
+
+    public String getPointsString() {
+        return pointMap.getOrDefault(points, "");
     }
 }
